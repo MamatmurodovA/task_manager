84b7a48421e8b2ed4aea06767c323c39712c61d6
A'zam Mamatmurodov <azam.mamatmurodov@gmail.com>
2019-02-02 14:23:07 +0500

    Added base controller and added controllers

--- core/controller.class.php
+++ core/controller.class.php
@@ -1,11 +1,29 @@
 <?php
 
 
-class BaseController 
+class Controller
 {
     public function __construct()
     {
-        
+        $page_name = ucfirst(strtolower($_GET['page']));
+        $class_name = "${page_name}Controller";
+        $class_path = "controllers/${class_name}.php";
+
+        if (realpath($class_path))
+        {
+            include $class_path;
+
+            $method_name = 'index';
+            if(isset($_GET['method']))
+            {
+                $method_name = strtolower($_GET['method']);
+            }
+            $obj = new $class_name;
+            if (method_exists($obj, $method_name)){
+                $obj->$method_name();
+            }
+        }
+
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('ROOT_DIR', __DIR__);
+
+include 'core/controller.class.php';
+
+$cntr = new Controller();
